32bafeae65d5a0268832974bb062cdbfd2f9795a
stanislav.zuiko <stanislav.zuiko@unity-bars.com>
2018-02-09 15:59:14 +0200

    Add project files.

---
+++ uksl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="uksl.MvcApplication" Language="C#" %>
---
+++ uksl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uksl.Startup))]
+namespace uksl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
